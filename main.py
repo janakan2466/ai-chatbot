4a62a445632f73106870a8e609705f4c124d8b9d
Janakan <janakan2466@gmail.com>
2021-10-11 03:57:38 -0400

    additional changed

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+#This chatbot allows modifications of intents
 import nltk
 from nltk.stem.lancaster import LancasterStemmer
 stemmer = LancasterStemmer()
@@ -18,21 +19,59 @@ docs_y= []
 
 #loop through all the intents within the Intent JSON file.
 for intent in data["intents"]:
+    #patterns are the user input
     for pattern in intent["patterns"]:
-        #tokenize all the words in each pattern key (returns a list)
-        wrds = nltk.word_tokenize(pattern)
-        words.extend(wrds)
-        docs_x.append(pattern)
+        #tokenize all the words in each pattern key (returns a list); the lines below stemming each pattern by detecting the root word 
+        #This is necessary to train the deep learning model
+        wrds = nltk.word_tokenize(pattern) #returns a list
+        words.extend(wrds) #adds to a list
+        docs_x.append(wrds)
         docs_y.append(intent["tag"])
-
+    #responses are the output that are trained from the JSON file
     if intent["tag"] not in labels:
         labels.append(intent["tag"])
         #stemming breaks sentences down to root words when which helps training the bot (makes the model more better)
         #tokenize the data
 
-words = [stemmer.stem(w.lower()) for w in words]
+words = [stemmer.stem(w.lower()) for w in words if w != "?"] #the question mark does not really have any value
 words = sorted(List(set(words)))
 
 labels = sorted(labels)
 
+#one hot encoding; tracking the amount of words that will occur (frequency) to the neural network
+training = []
+output= []
 
+out_empty= [0 for _ in range(len(labels))]
+
+for x, doc in enumerate(docs_x):
+    bag= []
+    wrds= [stemmer.stem(w.lower()) for w in doc]
+
+    for w in words:
+        if w in wrds:
+            bag.append(1)
+        else:
+            bag.append(0)
+
+    output_row= out_empty[:]
+    output_row[labels.index(docs_y[x])] = 1
+
+    training.append(bag)
+    output.append(output_row)
+
+training= numpy.array(training)
+output= numpy.array(output)
+
+tensorflow.reset_default_graph()
+
+net = tflearn.input_data(shape=[None, len(training[0])])
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, 8)
+net = tflearn.fully_connected(net, len(output[0]), activation="softmax")
+net = tflearn.regression(net)
+
+model = tflearn.DNN(net)
+
+model.fit(training, output, n_epoch=1000, batch_size=8, show_metric=True)
+model.save("model.tflearn")
